a33fe2c9ccd9fafa68fabf69ee4218b3b3d3fa16
unknown <dcasemhrz@gmail.com>
2024-08-14 00:50:35 +1000

    project completed

--- obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d2345bd7eb5e1b50714a702c7907b1ed9f4fdf1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
